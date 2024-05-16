0055869e435be9832f34336ec59071dea8c56b3c
jonasdspereira <jnsprr@outlook.com>
2024-05-16 05:27:56 -0400

    Exercicios modulo E - tabuada e contador

---
+++ MODULO-E/AULA13/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1;
+
+// // enquanto faça
+// while (c <= 6) {
+//   console.log(`passo ${c}`);
+//   c++;
+// }
+
+// faça enquanto
+do {
+  console.log(`passo ${c}`);
+  c++
+} while (c <= 6);
+
+// para
